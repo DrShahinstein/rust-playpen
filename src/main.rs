e51e5d60e2f203fe3355aae5fca03d09e9f029f2
DrShahinstein <1taylanozturk@protonmail.com>
2023-06-19 13:17:10 +0300

    Import the new module generic_methods inside main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 #[allow(unused_imports)]
 use rust_playpen::snippets::{
-    employee_automation, guess_number, median_and_mode, structs, temperature_converter, using_json,
+    employee_automation, generic_methods, guess_number, median_and_mode, structs,
+    temperature_converter, using_json,
 };
 
 fn main() {
-    todo!("Run code snippets to see how they work.");
-    // temperature_converter::run();
+    generic_methods::run();
+    // Test code snippets like the above:: __::run()
 }
