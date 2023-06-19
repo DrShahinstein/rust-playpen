d8ee79c2483d7046f3403886c163969a6c4149f5
DrShahinstein <1taylanozturk@protonmail.com>
2023-06-19 15:06:27 +0300

    Include the new module lifetimes.rs in the lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod snippets {
     pub mod generic_methods;
     pub mod guess_number;
+    pub mod lifetimes;
     pub mod median_and_mode;
     pub mod structs;
     pub mod temperature_converter;
