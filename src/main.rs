d691e3a8e213e914744cc6150c1f149f0ff2946c
DrShahinstein <1taylanozturk@protonmail.com>
2023-06-19 15:07:29 +0300

    Import the new module lifetimes.rs inside main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[allow(unused_imports)]
 use rust_playpen::snippets::{
-    employee_automation, generic_methods, guess_number, median_and_mode, structs,
+    employee_automation, generic_methods, guess_number, lifetimes, median_and_mode, structs,
     temperature_converter, using_json,
 };
 
 fn main() {
-    generic_methods::run();
+    lifetimes::run();
     // Test code snippets like the above:: __::run()
 }
