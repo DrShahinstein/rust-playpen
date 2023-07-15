4592a1ce895ebc73fa10e91d7731543ba47323b8
DrShahinstein <1taylanozturk@protonmail.com>
2023-07-15 23:21:07 +0300

    Import the new codewars kata in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 #[allow(unused_imports)]
 use rust_playpen::snippets::{
-    employee_automation, generic_methods, guess_number, lifetimes, median_and_mode, structs,
-    temperature_converter, using_json,
+    codewars, employee_automation, generic_methods, guess_number, lifetimes, median_and_mode,
+    structs, temperature_converter, using_json,
 };
 
+// Run snippets here
 fn main() {
-    lifetimes::run();
-    // Test code snippets like the above:: __::run()
+    /*
+    employee_automation::automation::run();
+    temperature_converter::run();
+    codewars::move_zeros_to_end::prove();
+    ...
+    */
 }
