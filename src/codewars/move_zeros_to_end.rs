3f0c8eb06c8ca971d27efcf89993be80289688ed
DrShahinstein <1taylanozturk@protonmail.com>
2023-07-17 23:20:12 +0300

    Refactor the prove() of kata

--- src/codewars/move_zeros_to_end.rs
+++ src/codewars/move_zeros_to_end.rs
@@ -6,6 +6,12 @@ pub fn move_zeros(arr: &[u8]) -> Vec<u8> {
     non_zeros.into_iter().chain(zeros.into_iter()).collect()
 }
 
+fn test(arg: &[u8], expected: &[u8]) {
+    let result = move_zeros(arg);
+    println!("{:?} -> {:?}", result, expected);
+    assert!(result == expected);
+}
+
 pub fn prove() {
     let arr1 = &[1, 2, 0, 1, 0, 1, 0, 3, 0, 1];
     let arr2 = &[9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9];
@@ -13,15 +19,9 @@ pub fn prove() {
     let arr4 = &[0];
     let arr5 = &[];
 
-    let result1 = move_zeros(arr1);
-    let result2 = move_zeros(arr2);
-    let result3 = move_zeros(arr3);
-    let result4 = move_zeros(arr4);
-    let result5 = move_zeros(arr5);
-
-    println!("{:?}", result1); // Output: [1, 2, 1, 1, 3, 1, 0, 0, 0, 0]
-    println!("{:?}", result2); // Output: [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    println!("{:?}", result3); // Output: [0, 0]
-    println!("{:?}", result4); // Output: [0]
-    println!("{:?}", result5); // Output: []
+    test(arr1, &[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+    test(arr2, &[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+    test(arr3, &[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+    test(arr4, &[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
+    test(arr5, &[1, 2, 1, 1, 3, 1, 0, 0, 0, 0]);
 }
