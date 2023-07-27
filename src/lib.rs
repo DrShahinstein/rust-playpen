fdb7560eebcb28333dbe16910638556f10c02fad
DrShahinstein <1taylanozturk@protonmail.com>
2023-07-27 23:34:58 +0300

    Add smart_pointers to the lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ pub mod snippets {
     pub mod guess_number;
     pub mod lifetimes;
     pub mod median_and_mode;
+    pub mod smart_pointers;
     pub mod structs;
     pub mod temperature_converter;
     pub mod using_json;
